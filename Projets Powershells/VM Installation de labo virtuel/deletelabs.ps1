2cdc66bdb5261ebc7ee5561a55146d221425fba0
Alain Philip Despont <alain.despont@outlook.com>
2023-11-26 15:09:39 +0100

    Removed

--- Projets Powershells/VM Installation de labo virtuel/deletelabs.ps1
+++
@@ -1,4 +0,0 @@
-#Suppression d'ancien labos
-get-vm -name DEPLOY* | Remove-VM
-Get-Item -Path "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\DEPLOY*" | Remove-Item
-Remove-VMSwitch "DEPLOY_PrivateSwitch"
